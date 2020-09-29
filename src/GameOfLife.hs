77935f4b8b365a3e0df940edc9ae983c29997cdd
Andrey Krainyak <mode89@mail.ru>
2020-09-29 14:28:50 +0800

    Overcrowding rule
    Alive cell with many live neighbours must die.

--- src/GameOfLife.hs
+++ src/GameOfLife.hs
@@ -8,4 +8,5 @@ data CellState = Dead | Alive deriving (Eq, Show)
 cellState :: CellState -> Int -> CellState
 cellState Alive liveNeighboursNumber
     | liveNeighboursNumber < 2 = Dead
+    | liveNeighboursNumber > 3 = Dead
     | otherwise = Alive
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,10 +4,18 @@ import Test.Tasty.HUnit
 
 main = defaultMain tests
 
-tests = testGroup "Tests" [ testUnderpopulationRule ]
+tests = testGroup "Tests"
+    [ testUnderpopulationRule
+    , testOvercrowdingRule ]
 
 testUnderpopulationRule = testCase "Underpopulation rule" $
     let liveNeighboursNumber = 0
+        prevCellState = Alive
+        newCellState = cellState prevCellState liveNeighboursNumber
+    in assertEqual "Cell must die" newCellState Dead
+
+testOvercrowdingRule = testCase "Overcrowding rule" $
+    let liveNeighboursNumber = 4
         prevCellState = Alive
         newCellState = cellState prevCellState liveNeighboursNumber
     in assertEqual "Cell must die" newCellState Dead
