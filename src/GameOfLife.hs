afae1cedc57c8f9414420af73f8419c585f5bfb1
Andrey Krainyak <mode89@mail.ru>
2020-09-29 18:32:26 +0800

    Sliding window on a grid
    Going to use sliding window to count cell neighbours.

--- src/GameOfLife.hs
+++ src/GameOfLife.hs
@@ -5,6 +5,8 @@ module GameOfLife
     , gridLiveNeighboursNumber
     , gridState
     , slidingWindow
+    , slidingWindowThroughColumns
+    , slidingWindow2d
     ) where
 
 import qualified Data.List as List
@@ -32,3 +34,12 @@ slidingWindow windowSize xs =
     let listLenght = List.length xs
         windowNum = listLenght - (windowSize - 1)
     in take windowNum . List.transpose . take windowSize . List.tails $ xs
+
+slidingWindowThroughColumns :: Int -> [[a]] -> [[[a]]]
+slidingWindowThroughColumns windowSize grid =
+    List.transpose $ map (slidingWindow windowSize) grid
+
+slidingWindow2d :: Int -> [[a]] -> [[[a]]]
+slidingWindow2d windowSize grid =
+    let rowWindows = slidingWindow windowSize grid
+    in concat $ map (slidingWindowThroughColumns windowSize) rowWindows
--- test/Spec.hs
+++ test/Spec.hs
@@ -13,6 +13,11 @@ tests = testGroup "Tests"
     , testSingularGridLiveNeighboursNumber
     , testSlidingWindow
     , testSlidingWindowLongerList
+    , testSlidingWindow3x2
+    , testSlidingWindowThroughColumns
+    , testSlidingWindow2x3
+    , testSlidingWindow3x3
+    , testSlidingWindow3x3_WindowSize3
     ]
 
 testUnderpopulationRule = testCase "Underpopulation rule" $
@@ -51,3 +56,49 @@ testSlidingWindow = testCase "Sliding window" $
 
 testSlidingWindowLongerList = testCase "Sliding window on longer list" $
     [[0, 1], [1, 2], [2, 3], [3, 4]] @=? slidingWindow 2 [0, 1, 2, 3, 4]
+
+testSlidingWindow3x2 = testCase "Sliding window on 3x2 grid" $
+    [[[1, 2], [3, 4]], [[3, 4], [5, 6]]] @=?
+        slidingWindow2d 2 [[1, 2], [3, 4], [5, 6]]
+
+testSlidingWindowThroughColumns = testCase "Sliding window through columns" $
+    let input = [[1, 2, 3],
+                 [4, 5, 6]]
+        output = [[[1, 2],
+                  [4, 5]],
+                 [[2, 3],
+                  [5, 6]]]
+    in output @=? slidingWindowThroughColumns 2 input
+
+testSlidingWindow2x3 = testCase "Sliding window on 2x3 grid" $
+    let input = [[1, 2, 3],
+                 [4, 5, 6]]
+        output = [[[1, 2],
+                   [4, 5]],
+                  [[2, 3],
+                   [5, 6]]]
+    in output @=? slidingWindow2d 2 input
+
+testSlidingWindow3x3 = testCase "Sliding window on 3x3 grid" $
+    let input = [[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 9]]
+        output = [[[1, 2],
+                   [4, 5]],
+                  [[2, 3],
+                   [5, 6]],
+                  [[4, 5],
+                   [7, 8]],
+                  [[5, 6],
+                   [8, 9]]]
+    in output @=? slidingWindow2d 2 input
+
+testSlidingWindow3x3_WindowSize3 =
+    testCase "Sliding window on 3x3 grid with window size 3" $
+        let input = [[1, 2, 3],
+                     [4, 5, 6],
+                     [7, 8, 9]]
+            output = [[[1, 2, 3],
+                       [4, 5, 6],
+                       [7, 8, 9]]]
+        in output @=? slidingWindow2d 3 input
