f3c4cb591d704ba5714d13f1917d9593b7bdb63d
Andrey Krainyak <mode89@mail.ru>
2020-09-30 15:36:10 +0800

    Textual frontend
    Evolve r-pentomino and output result into terminal.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Control.Concurrent
+import GameOfLife.Core
+
 main :: IO ()
-main = putStrLn "Not yet implemented"
+main = do
+    let initialGrid = embedGrid (emptyGrid 23 79) 10 38 rPentomino
+    mainLoop initialGrid
+
+mainLoop :: Grid -> IO ()
+mainLoop grid = do
+    putStrLn ""
+    putStrLn $ stringFromGrid grid
+    let nextGrid = nextGridState grid
+    threadDelay 100000
+    mainLoop nextGrid
