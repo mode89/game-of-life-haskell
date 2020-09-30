946a0770d5548f5e20495b35fc1e49520c9202ca
Andrey Krainyak <mode89@mail.ru>
2020-09-30 16:59:55 +0800

    Move TUI to GameOfLife.UI.Terminal
    Going to introduce GTK version of UI as an option.

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,6 @@
 module Main where
 
-import Control.Concurrent
-import GameOfLife.Core
+import qualified GameOfLife.UI.Terminal as UI
 
 main :: IO ()
-main = do
-    let initialGrid = embedGrid (emptyGrid 23 79) 10 38 rPentomino
-    mainLoop initialGrid
-
-mainLoop :: Grid -> IO ()
-mainLoop grid = do
-    putStrLn ""
-    putStrLn $ stringFromGrid grid
-    let nextGrid = nextGridState grid
-    threadDelay 100000
-    mainLoop nextGrid
+main = UI.main
