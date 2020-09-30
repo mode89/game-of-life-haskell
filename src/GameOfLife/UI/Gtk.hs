f0d945614d13432451f3d098dc24c0b5569762e3
Andrey Krainyak <mode89@mail.ru>
2020-09-30 17:30:13 +0800

    Disable OverloadedLabels extension
    Learning to use plain language.

--- src/GameOfLife/UI/Gtk.hs
+++ src/GameOfLife/UI/Gtk.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module GameOfLife.UI.Gtk ( main ) where
@@ -7,11 +6,13 @@ import Control.Concurrent
 import Data.GI.Base
 import GameOfLife.Core
 import qualified GI.Gtk as Gtk
+import qualified GI.Gtk.Enums as GtkEnums
 
 main :: IO ()
 main = do
     Gtk.init Nothing
-    win <- new Gtk.Window [#title := "Introduction"]
-    on win #destroy Gtk.mainQuit
-    #showAll win
+    win <- Gtk.windowNew GtkEnums.WindowTypeToplevel
+    Gtk.windowSetTitle win "Game of Life"
+    Gtk.onWidgetDestroy win Gtk.mainQuit
+    Gtk.widgetShowAll win
     Gtk.main
